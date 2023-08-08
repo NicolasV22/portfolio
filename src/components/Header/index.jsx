3199737830937b98486575b2336283466160a714
Nicolas <nicolasverez2204@gmail.com>
2023-08-08 19:44:08 +0200

    avancement header fini

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import { Link }  from 'react-router-dom'
+import '../../styles/Header.scss'
+import logo from '../../images/logo.png'
+
+function Header(){
+    return (
+        <header className='header'>
+            <img src={logo} alt="logo du site kasa" className='header__logo'/>
+            <nav className='header__navbar'>
+                <Link to="/" className='header__navbar__link'>Accueil</Link>
+                <Link to="/about" className='header__navbar__link'>A Propos</Link>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import Home from './pages/Home/';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import About from './pages/About/';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/'
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <Router>
+      <Header />
       <Routes>
-
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
-    
-  </React.StrictMode>,
-  document.getElementById('root')
-)
-
+  </React.StrictMode>
+);
 
